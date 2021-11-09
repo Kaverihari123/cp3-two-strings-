2059c3dafa6db2ebab9e8bb3e56e72ff4c0002bb
Kaveri <kav erihari123@gmail.com>
2021-11-09 03:43:13 +0000

    two strings

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const a = app.get("/", (request, response) => {
+  response.send("Home Page");
+});
+
+const b = app.get("/about", (request, response) => {
+  response.send("About Page");
+});
+module.exports = a;
+module.exports = b;
+app.listen(3000);
